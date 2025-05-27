3652107a4b7bea05a0f81b10233415cab76e26c0
ManiacJack82 <de.berlin@gmx.de>
2025-05-27 15:54:02 +0200

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,16 @@
+// js/firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1HsJvWIYnBPCqSQS0ymOL0IEdZW8KDOY",
+  authDomain: "trainings-app-24eb7.firebaseapp.com",
+  projectId: "trainings-app-24eb7",
+  storageBucket: "trainings-app-24eb7.appspot.com",
+  messagingSenderId: "266317486009",
+  appId: "1:266317486009:web:730e7f91eaae69fb584868",
+  measurementId: "G-3EWQ7W9Z85"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
