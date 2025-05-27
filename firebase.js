f6f6dd376bba1ccee8d1eea8e1afec502bce24d3
ManiacJack82 <de.berlin@gmx.de>
2025-05-27 12:52:40 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Auth Listener
+firebase.auth().onAuthStateChanged(user => {
+  if (!user && location.pathname.includes("app.html")) {
+    location.href = "index.html";
+  }
+});
 
