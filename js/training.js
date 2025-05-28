86daa15fe2ec03899d32a2c886fb2a3c5584552b
ManiacJack82 <de.berlin@gmx.de>
2025-05-28 14:31:06 +0200

    Create training.js

---
+++ js/training.js
@@ -0,0 +1,18 @@
+<div class="exercise-section">
+  <button id="toggle-list">Übungen anzeigen/verbergen</button>
+
+  <div id="exercise-list" style="display: block;"></div>
+
+  <select id="exercise-select">
+    <option value="">Übung auswählen</option>
+    <option value="Bankdrücken">Bankdrücken</option>
+    <option value="Kniebeugen">Kniebeugen</option>
+    <option value="Kreuzheben">Kreuzheben</option>
+  </select>
+
+  <input type="number" id="weight-input" placeholder="Gewicht (kg)" />
+  <input type="number" id="reps-input" placeholder="Wiederholungen" />
+  <button id="add-exercise">Übung hinzufügen</button>
+
+  <div id="last-values"></div>
+</div>
